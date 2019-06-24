9c0463edd4874a581144ca01f3cb4949f529ca15
Neel Patel <31348835+iNeelPatel@users.noreply.github.com>
2019-06-24 18:07:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $timestamp = date("d-m-Y - H:i:s");
 $datastring = $_GET["data1"];
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = $timestamp . " - ".$data1."\n";
+$txt = $timestamp . " - ".$datastring."\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 echo $txt;
